58342c40eb243f07e027a0fee0ca4271f4776781
xiaoshijiu <mychenshan@126.com>
2019-07-08 23:33:43 +0800

    消费端添加监控actuator依赖，以及注册一个Hystrix Dashboard图形化依赖

---
+++ springcloud-feign-client/src/main/java/com/fei/springcloudfeignclient/config/DashBoardConfig.java
@@ -0,0 +1,25 @@
+package com.fei.springcloudfeignclient.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: xiaoshijiu
+ * @Date: 2019/7/8
+ * @Description: 注册一个Servlet，开启Hystrix DashBoard图形化监控
+ */
+@Configuration
+public class DashBoardConfig {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
